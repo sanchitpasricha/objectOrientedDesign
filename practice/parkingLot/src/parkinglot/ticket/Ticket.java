95dc99eebafadc15f8cd19a13182abbcb586b3dd
sanchitpasricha <pasrichasanchit@outlook.com>
2026-07-31 14:37:44 +0530

    ticket class

--- practice/parkingLot/src/parkinglot/ticket/Ticket.java
+++ practice/parkingLot/src/parkinglot/ticket/Ticket.java
@@ -1,5 +1,46 @@
 package practice.parkingLot.src.parkinglot.ticket;
 
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import parkinglot.parkingspot.ParkingSpot;
+import parkinglot.vehicle.Vehicle;
+
 public class Ticket {
-    
+    private final String ticketId;
+    private final Vehicle vehicle;
+    private final ParkingSpot parkingSpot;
+    private final LocalDateTime entryTime;
+    private LocalDateTime exitTime;
+
+    public Ticket(String ticketId, Vehicle vehicle, ParkingSpot parkingSpot, LocalDateTime entryTime){
+        this.ticketId = ticketId;
+        this.vehicle = vehicle;
+        this.parkingSpot = parkingSpot;
+        this.entryTime = entryTime;
+        this.exitTime = null;
+    }
+
+    public BigDecimal calculateParkingDuration(){
+        return new BigDecimal(Duration.between(entryTime, Objects.requireNonNullElseGet(exitTime, LocalDateTime::now))
+                        .toMinutes());
+    }
+
+    public Vehicle getVehicle(){
+        return vehicle;
+    }
+
+     public LocalDateTime getEntryTime(){
+        return entryTime;
+    }
+
+    public LocalDateTime getExitTime(){
+        return exitTime;
+    }
+
+    public void setExitTime(LocalDateTime exitTime){
+        this.exitTime = exitTime;
+    }
 }
